608a8590646e362b1591ad8932972785a3e73bd4
Iryna Serhiienko <irishka.sergienko@gmail.com>
2023-03-24 14:44:28 +0200

    Added SSR to contacts  & SSG to posts

---
+++ next-js/pages/posts/index.js
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Heading from '@/components/Heading';
+
+export const getStaticProps = async () => { //SSR
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  //   const data = null;
+  const data = await response.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { posts: data },
+  };
+};
+
+export default function Posts({ posts }) {
+  return (
+    <>
+      <Head>
+        <title>Posts</title>
+      </Head>
+      <Heading text='Posts list:' />
+      <ul>
+        {posts &&
+          posts.map(({ id, title }) => (
+            <li key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+}
