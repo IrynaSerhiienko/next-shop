0241eeec476cc027aba0137a53ba10239052e51e
Iryna Serhiienko <irishka.sergienko@gmail.com>
2023-03-24 13:06:17 +0200

    Added mechanism SSR for page [id].js (single contact)

--- next-js/pages/contacts/[id].js
+++ next-js/pages/contacts/[id].js
@@ -2,6 +2,23 @@ import Head from 'next/head';
 import ContactInfo from '@/components/ContactInfo';
 import Heading from '@/components/Heading';
 
+export const getServerSideProps = async (context) => {  //SSR
+    // console.log(context);
+    const {id} = context.params;
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const data = await response.json();
+  
+    if (!data) {
+      return {
+        notFound: true,
+      };
+    }
+  
+    return {
+      props: { contact: data },
+    };
+  };
+
 export default function Contact({contact}) {
   return (
     <>
@@ -9,7 +26,6 @@ export default function Contact({contact}) {
         <title>Contact</title>
       </Head>
       <ContactInfo contact={contact}/>
-      {/* <Heading text='Jack' /> */}
     </>
   );
 }
