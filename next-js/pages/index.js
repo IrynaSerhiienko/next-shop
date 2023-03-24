83b3efe76faa5ca587c60ba52e5039bb50936cd0
Iryna Serhiienko <irishka.sergienko@gmail.com>
2023-03-24 21:51:11 +0200

    test

--- next-js/pages/index.js
+++ next-js/pages/index.js
@@ -30,7 +30,7 @@ export default function Home({ socials }) {
       <Head>
         <title>Home</title>
       </Head>
-      <Heading text='Hello world!' />
+      <Heading text='Hello world!!!!!!!' />
       <Socials socials={socials} />
     </div>
   );
