aced8b91441ee34bc95976b7f64be73c6e8cf572
Iryna Serhiienko <irishka.sergienko@gmail.com>
2023-03-24 21:16:28 +0200

    Changed socials

--- next-js/pages/api/data/socials.js
+++ next-js/pages/api/data/socials.js
@@ -1,27 +1,22 @@
 export const socials = [
-    {
-      id: 1,
-      icon: 'youtube',
-      path: 'https://youtube.com/YauhenKavalchuk',
-    },
-    {
-      id: 2,
-      icon: 'instagram',
-      path: 'https://instagram.com/YauhenKavalchuk',
-    },
-    {
-      id: 3,
-      icon: 'linkedin',
-      path: 'https://linkedin.com/in/YauhenKavalchuk',
-    },
-    {
-      id: 4,
-      icon: 'vk',
-      path: 'https://vk.com/YauhenKavalchuk',
-    },
-    {
-      id: 5,
-      icon: 'twitter',
-      path: 'https://twitter.com/YauhenKavalchuk',
-    },
-  ];
+  {
+    id: 1,
+    icon: 'youtube',
+    path: '',
+  },
+  {
+    id: 2,
+    icon: 'instagram',
+    path: '',
+  },
+  {
+    id: 3,
+    icon: 'linkedin',
+    path: '',
+  },
+  {
+    id: 4,
+    icon: 'facebook',
+    path: '',
+  },
+];
