a387a43d182026b03397d19723881c57e89df3f4
Iryna Serhiienko <irishka.sergienko@gmail.com>
2023-03-25 12:56:09 +0200

    Changed name env

--- next-js/pages/index.js
+++ next-js/pages/index.js
@@ -1,28 +1,46 @@
+import axios from 'axios';
 import Head from 'next/head';
+import s from '@/styles/Home.module.scss';
 import Heading from '@/components/Heading';
 import Socials from '@/components/Socials';
-import s from '@/styles/Home.module.scss';
 
 export const getStaticProps = async () => {
   try {
-    const response = await fetch(`${process.env.API_HOST}/api/socials/`);
-    const data = await response.json();
-
-    if (!data) {
-      return {
-        notFound: true,
-      };
-    }
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_VERCEL_ENV}/socials/`);
+    // const response = await axios.get(`${process.env.API_HOST}/socials/`);
+    const socials = response.data;
 
     return {
-      props: { socials: data },
+      props: { socials },
     };
-  } catch {
+  } catch (error) {
+    console.error(error);
     return {
-      props: { socials: null },
+      notFound: true,
     };
   }
 };
+
+// export const getStaticProps = async () => {
+//   try {
+//     const response = await fetch(`${process.env.API_HOST}/socials/`);
+//     const data = await response.json();
+
+//     if (!data) {
+//       return {
+//         notFound: true,
+//       };
+//     }
+
+//     return {
+//       props: { socials: data },
+//     };
+//   } catch {
+//     return {
+//       props: { socials: null },
+//     };
+//   }
+// };
 
 export default function Home({ socials }) {
   return (
