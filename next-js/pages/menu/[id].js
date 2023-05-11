0b1be82f669b56066a1f7f0c97066a7684eff52d
Iryna Serhiienko <irishka.sergienko@gmail.com>
2023-05-11 14:47:58 +0300

    test

--- next-js/pages/menu/[id].js
+++ next-js/pages/menu/[id].js
@@ -1,88 +1,92 @@
-// import axios from 'axios';
+import axios from 'axios';
 import Head from 'next/head';
-import Image from 'next/image';
 import CardDetails from '@/components/CardDetails';
+
 import s from '@/styles/Card.module.scss';
 
 export const getStaticPaths = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_ENV}/menu/`);
-  const data = await response.json();
+  try {
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_VERCEL_ENV}/menu/`);
+    const data = response.data;
 
-  const paths = data.map((cake) => {
+    const paths = data.map((cake) => {
+      return {
+        params: { id: cake.id.toString() },
+      };
+    });
+    //console.log('paths>>>', paths);
     return {
-      //params: { id: cake.id },
-      params: { id: cake.id.toString() },
+      paths,
+      fallback: false,
     };
-  });
-
-  //console.log('paths>>>', paths);
-
-  return {
-    paths,
-    fallback: false,
-  };
+  } catch (error) {
+    console.error(error);
+    return {
+      notFound: true,
+    };
+  }
 };
 
 export const getStaticProps = async (context) => {
-  //   console.log('context >>>', context);
-  //   console.log('context.params >>>', context.params);
-  //   console.log('context.params.id >>>', context.params.id);
-  let id = context.params.id;
-  const response = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_ENV}/menu?id=${id}`);
-  const data = await response.json();
-  const cake = data.find((cake) => cake.id.toString() === id);
+  try {
+    let id = context.params.id;
+    // const { id } = context.params;
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_VERCEL_ENV}/menu?id=${id}`);
+    const data = response.data;
+    const cake = data.find((cake) => cake.id.toString() === id);
 
-  return {
-    props: { cake },
-    // props: { cake: data },
-  };
+    return {
+      props: { cake },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      notFound: true,
+    };
+  }
 };
 
 export default function Details({ cake }) {
   return (
-    <div className={s.wrapper}>
-      <CardDetails cake={cake} />
-    </div>
-
-    // <>
-    //   <Head>
-    //     <title>Cake</title>
-    //   </Head>
-    //   <h1>Details</h1>
-
-    //   <div>
-    //     <Image
-    //       alt={cake.name || 'Cake image'}
-    //       className={s.img}
-    //       src={cake.image}
-    //       width={266}
-    //       height={404}
-    //       priority={true}
-    //       // loading='eager'
-    //       // style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }}
-    //     />
-    //   </div>
-    //   <strong className={s.name}>{cake.name}</strong>
-    //   <p className={`${s.price} mt-4`}>Price: {cake.price} UAH</p>
-    // </>
+    <>
+      <Head>
+        <title>Cake details</title>
+      </Head>
+      <div className={s.wrapper}>
+        <CardDetails cake={cake} />
+      </div>
+    </>
   );
 }
 
-// export const getStaticProps = async (context) => {
-//   try {
-//     const { id } = context.params;
-//     const response = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_ENV}/menu/${id}`);
-//     const data = await response.json();
-//     // const response = await axios.get(`${process.env.NEXT_PUBLIC_VERCEL_ENV}/menu/${id}`);
-//     // const data = response.data;
+// export const getStaticPaths = async () => {
+//   const response = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_ENV}/menu/`);
+//   const data = await response.json();
 
+//   const paths = data.map((cake) => {
 //     return {
-//       props: { cake: data },
+//       //params: { id: cake.id },
+//       params: { id: cake.id.toString() },
 //     };
-//   } catch (error) {
-//     console.error(error);
-//     return {
-//       notFound: true,
-//     };
-//   }
+//   });
+
+//   //console.log('paths>>>', paths);
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
+
+// export const getStaticProps = async (context) => {
+//   //   console.log('context.params >>>', context.params);
+//   //   console.log('context.params.id >>>', context.params.id);
+//   let id = context.params.id;
+//   const response = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_ENV}/menu?id=${id}`);
+//   const data = await response.json();
+//   const cake = data.find((cake) => cake.id.toString() === id);
+
+//   return {
+//     props: { cake },
+//     // props: { cake: data },
+//   };
 // };
--- next-js/pages/menu/index.js
+++ next-js/pages/menu/index.js
@@ -1,21 +1,42 @@
 import axios from 'axios';
 import Head from 'next/head';
-import Link from 'next/link';
-import Image from 'next/image';
 import MenuContent from '../../components/MenuContent';
 import MenuTitle from '@/components/MenuTitle';
-import CardDetails from '@/components/CardDetails';
 
 import s from '@/styles/Menu.module.scss';
 
 export const getStaticProps = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_ENV}/menu/`);
-  const data = await response.json();
+  try {
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_VERCEL_ENV}/menu/`);
+    const data = response.data;
 
-  return {
-    props: { menu: data },
-  };
+    return {
+      props: { menu: data },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      notFound: true,
+    };
+  }
 };
+
+export default function Menu({ menu }) {
+  return (
+    <>
+      <Head>
+        <title>Menu</title>
+      </Head>
+      <section className={s.menu}>
+        <div className={s.wrapper}>
+          <MenuTitle />
+          <div className={s.menuSection}>{menu && <MenuContent menu={menu} />}</div>
+        </div>
+      </section>
+    </>
+  );
+}
+
 // export const getStaticProps = async () => {
 //   try {
 //     const response = await axios.get(`${process.env.NEXT_PUBLIC_VERCEL_ENV}/menu/`);
@@ -32,28 +53,3 @@ export const getStaticProps = async () => {
 //     };
 //   }
 // };
-
-export default function Menu({ menu }) {
-  return (
-    <>
-      <section className={s.menu}>
-        <div className={s.wrapper}>
-          <Head>
-            <title>Menu</title>
-          </Head>
-          <MenuTitle />
-          {/* <ul>
-            {menu &&
-              menu.map(({ id, name }) => (
-                <li key={id}>
-                  <Link href={`/menu/${id}`}>{name}</Link>
-                </li>
-              ))}
-          </ul> */}
-
-          <div className={s.menuSection}>{menu && <MenuContent menu={menu} />}</div>
-        </div>
-      </section>
-    </>
-  );
-}
